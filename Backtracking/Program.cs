ecf01af63dd5f4097a0caabbcc095a1022523b9a
Karthik <karthikkottugumada@outlook.com>
2020-03-04 16:18:14 -0800

    Subsets

--- Backtracking/Program.cs
+++ Backtracking/Program.cs
@@ -7,6 +7,10 @@ namespace Backtracking
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Subsets sb = new Subsets();
+            // sb.Subsets_Iterative(new int[] { 1,2,3});
+            // sb.Subsets_Recursive(new int[] { 1, 2, 3 });
+            sb.SubsetsWithDup(new int[] {1, 2, 2});
         }
     }
 }
